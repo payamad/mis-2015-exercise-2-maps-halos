98b41f523b0e419b3cf09f2767bfa9c747f2a80b
payamad <p.adineh@gmail.com>
2015-05-08 19:11:32 +0200

    task 2a Show current location in map Add text box Add long press to add a new marker Add click listener on markers to show message box Save markers info in SharedPreferences

--- HelloMaps/app/src/main/java/com/example/mmbuw/hellomaps/MapsActivity.java
+++ HelloMaps/app/src/main/java/com/example/mmbuw/hellomaps/MapsActivity.java
@@ -1,21 +1,42 @@
 package com.example.mmbuw.hellomaps;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private LocationManager locationManager;
+    private SharedPreferences sharedPref;
+
+    private EditText editText;
+
+    private int markerCounter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        //setup location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        editText = (EditText) findViewById(R.id.edittext);
+
         setUpMapIfNeeded();
     }
 
@@ -60,6 +81,63 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        /**
+         * Set current location in map
+         * Source: http://stackoverflow.com/questions/2227292/how-to-get-latitude-and-longitude-of-the-mobiledevice-in-android
+         *
+         */
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(Integer.toString(markerCounter)));
+
+        // Save marker information
+        setNewMarkerToPreferences("Marker", latLng);
+
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerClickListener(this);
+    }
+
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        String message = editText.getText().toString();
+
+        // Add new marker in map
+        mMap.addMarker(new MarkerOptions().position(latLng).title(Integer.toString(markerCounter)));
+
+        // Save marker information
+        setNewMarkerToPreferences(message, latLng);
+    }
+
+    private void setNewMarkerToPreferences(String value, LatLng latLng){
+        /**
+         * Add message, latitude and longitude to SharedPreferences
+         * link -> https://developer.android.com/training/basics/data-storage/shared-preferences.html
+         */
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(Integer.toString(markerCounter), value);
+        editor.putString(markerCounter + "-LatLng", latLng.toString());
+        editor.commit();
+        markerCounter++;
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        /**
+         * read marker information from SharedPreferences
+         * and show a message box with message and latitude and longitude
+         */
+        int markerId = Integer.parseInt(marker.getTitle().toString());
+        String text = sharedPref.getString(Integer.toString(markerId),"") + "\n"
+                + sharedPref.getString(markerId + "-LatLng","");
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(text);
+        AlertDialog alertDialog = builder.create();
+
+        alertDialog.show();
+        return true;
     }
 }
